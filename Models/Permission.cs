6fd0f6c57544e2b985e1d3a0e6205630927e3ddb
Boss Codes <jadsaderkarate@gmail.com>
2024-08-04 22:11:36 +0300

    Fixed permission model

---
+++ Models/Permission.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory_Management_Backend.Models
+{
+    public class UserPermission
+    {
+        public int PermissionID { get; set; }
+        public string Permission { get; set; }
+    }
+}
